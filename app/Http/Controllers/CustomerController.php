0fe1a92610ae65ac465e44fcb4be2abfdf199b2e
devlob <devlobb@gmail.com>
2022-06-28 11:00:29 -0700

    fix code issue

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -5,25 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Customer;
 
-
-
 class CustomerController extends Controller
 {
-    public function index(){
-      
-      return view('customer');
-    }
+  public function index()
+  {
+    return view('customer');
+  }
 
-    public function store(Request $request)
-    {
-      dd($request->email);
+  public function store(Request $request)
+  {
+    dd($request->email);
     $customer = new Customer;
-    $customer->email=$request->'email';
-    $customer->password=$request->'password';
-    $customer->address=$request->'address';
-    $customer->city=$request->'city';
-    $customer->state=$request->'state';
-    $customer->zip=$request->'zip';
+    $customer->email = $request->email;
+    $customer->password = $request->password;
+    $customer->address = $request->address;
+    $customer->city = $request->city;
+    $customer->state = $request->state;
+    $customer->zip = $request->zip;
     $customer->save();
-    }
+  }
 }
